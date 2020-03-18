84f3e478deb9d41a60d6507ab4ba56dac1e8d558
Alexander Colen <alexanderc@live.nl>
2020-03-18 17:49:57 -0400

    Added pdf split functionality.

--- PdfEditor.py
+++ PdfEditor.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileReader, PdfFileMerger, PdfFileWriter
+from PyPDF2 import PdfFileReader, PdfFileWriter
 import sys
 
 
@@ -7,50 +7,53 @@ def handle_command(args: []):
     Handle the command line arguments and redirect them to the proper functions.
     :param args: The arguments that were used to call this script.
     """
-    if len(args) < 2:
+
+    command: str = args[1]
+
+    if command == 'help':
+        print('----------------------\nScript usage commands\n----------------------\n'
+              f'# {args[0]} help\n'
+              f'# {args[0]} read <Path/To/Pdf.pdf>\n'
+              f'  Example usage: {args[0]} read JobOfferContract.pdf\n'
+              f'# {args[0]} merge <Path/To/Pdf1.pdf> <Path/To/Pdf2.pdf> <Path/To/Pdf3.pdf> ...\n'
+              f'  Example usage: {args[0]} merge scan1.pdf scan2.pdf scan3.pdf\n'
+              f'# {args[0]} split <Path/To/Pdf.pdf>\n'
+              f'  Example usage: {args[0]} split ScannedFiles.pdf\n')
+        return
+
+    if len(args) < 3:
         print(f"Invalid script usage. Check commands with '{args[0]} help'.")
         return
 
-    command: str = args[1]
-    if len(args) >= 3:
-        path: str = args[2]
-
-    if command == 'help':
-        print('======================\nScript usage commands\n======================')
-        print(f'# {args[0]} help')
-        print(f'# {args[0]} read <Path/To/Pdf.pdf>')
-        print(f'  Example usage: {args[0]} read JobOfferContract.pdf')
-        print(f'# {args[0]} merge <Path/To/Pdf1.pdf> <Path/To/Pdf2.pdf> <Path/To/Pdf3.pdf> ...')
-        print(f'  Example usage: {args[0]} merge scan1.pdf scan2.pdf scan3.pdf')
-        print(f'# {args[0]} split <Path/To/Pdf.pdf> <(Optional) Page number>')
-        print(f'  Example usage: {args[0]} split ScannedFiles.pdf 13')
-    elif command == 'read':
-        print('Attempting to extract PDF information...')
-        extract_information(pdf_path=path)
+    if command == 'read':
+        print('Attempting to extract PDF metadata...')
+        extract_metadata(pdf_path=args[2])
+        return
     elif command == 'merge':
         print('Attempting to merge PDF files...')
         merge_pdf(args[2:])
+        return
     elif command == 'split':
         print('Attempting to split PDF file(s)...')
-        if len(args) == 4 and isinstance(args[3], int):
-            # Split with giving split page.
-            split_pdf(pdf_path=path, page=args[3])
-        else:
-            # Split without giving split page.
-            split_pdf(pdf_path=path)
+        split_pdf(pdf_path=args[2])
+        return
+
+    print(f"Invalid script usage. Check commands with '{args[0]} help'.")
 
 
-def extract_information(pdf_path: str):
+def extract_metadata(pdf_path: str):
     """
-    Extract information from a PDF file and displays it on the command line.
+    Extract metadata from a PDF file and displays it on the command line.
     :param pdf_path: The path to the PDF file.
-    :return The information belonging to the PDF file.
     """
     try:
+        if not pdf_path.endswith('.pdf'):
+            pdf_path += '.pdf'
+
         with open(pdf_path, 'rb') as f:
-            pdf: PdfFileReader = PdfFileReader(f)
-            information = pdf.getDocumentInfo()
-            number_of_pages: int = pdf.getNumPages()
+            pdf_reader: PdfFileReader = PdfFileReader(f)
+            information = pdf_reader.getDocumentInfo()
+            pages = pdf_reader.getNumPages()
 
         txt = f"Information about {pdf_path}: \n" \
             f"  Author: {information.author}\n" \
@@ -58,10 +61,9 @@ def extract_information(pdf_path: str):
             f"  Producer: {information.producer}\n" \
             f"  Subject: {information.subject}\n" \
             f"  Title: {information.title}\n" \
-            f"  Number of pages: {number_of_pages}"
+            f"  Number of pages: {pages}"
 
         print(txt)
-        return information
     except FileNotFoundError:
         print(f"File '{pdf_path}' not found. Double check if the file exists in the specified location.")
 
@@ -76,35 +78,68 @@ def merge_pdf(files: []):
     # Define output filename ending in .pdf
     output: str = input('Enter filename for output:\n>>> ')
     if not output:
-        output = 'merged'
-    if not output.endswith('.pdf'):
+        output = 'merged.pdf'
+    elif not output.endswith('.pdf'):
         output += '.pdf'
 
     for f in files:
+        if not f.endswith('.pdf'):
+            f += '.pdf'
         try:
-            pdf_reader = PdfFileReader(f)
+            pdf_reader: PdfFileReader = PdfFileReader(f)
             for page in range(pdf_reader.getNumPages()):
                 pdf_writer.addPage(pdf_reader.getPage(page))
         except FileNotFoundError:
             print(f"File '{f}' not found. Double check if the file exists in the specified location.")
 
+    # TODO: Check if files were found before trying to save in case of empty/unfound files.
     # Write & Save the merged PDF.
     with open(output, 'wb') as out:
         pdf_writer.write(out)
         print(f"Merged existing files together as '{output}'.")
 
 
-def split_pdf(pdf_path: str, page: int = 0):
+def split_pdf(pdf_path: str):
     """
-    Split a PDF at a certain page and save the results as two new PDF files.
+    Split a PDF at certain pages and save the results as new PDF files.
     :param pdf_path: The path to the PDF.
-    :param page: The page number where to split. Defaults to 0.
     """
-    if not page:
-        page = input('At what page do you want to split this document?\n>>> ')
+    try:
+        if not pdf_path.endswith('.pdf'):
+            pdf_path += '.pdf'
 
-    # TODO: Write split functionality.
-    print('NOT YET IMPLENTED. Terminating program.')
+        # Try to read the file first to check if it exists.
+        pdf_reader: PdfFileReader = PdfFileReader(pdf_path)
+        pdf_length: int = pdf_reader.getNumPages()
+
+        # Ask what page the user wants to split the PDF.
+        pages: [] = [int(input('After which page do you want to split this document?\n>>> ')) - 1]
+
+        while True:
+            if input('Would you like to split it again after another page? (y/n)\n>>> ').lower() in ['y', 'yes']:
+                page: int = int(input('After which page do you want to split this document?\n>>> ')) - 1
+                if pdf_length > page:
+                    pages.append(page)
+                else:
+                    print(f'Cannot split after {page} because the selected PDF only has {pdf_length} pages.')
+            else:
+                pages.append(pdf_length - 1)
+                break
+
+        pdf_writer: PdfFileWriter = PdfFileWriter()
+        split: int = 1
+        for page in range(pdf_length):
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+            if page in pages:
+                with open(f'split{split}.pdf', 'wb') as out:
+                    pdf_writer.write(out)
+                    print(f"Merged existing files together as 'split{split}'.")
+                # Re-initiate the writer to empty the pages and increment the split counter.
+                pdf_writer = PdfFileWriter()
+                split += 1
+    except FileNotFoundError:
+        print(f"File '{pdf_path}' not found. Double check if the file exists in the specified location.")
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Command-Line-PDF-Editor
 Command line PDF editor written in Python3.
 
+PDF utility suit allowing easy merging, splitting and simple editing of PDF files.
+
 To run the project, clone the repository or download the PdfEditor.py file from this project.
-It can be executed using Python3 by running: `python3 PdfEditor.py`.
+It can be executed using Python3 by running: `python3 PdfEditor.py <command>`.
 
 Available commands:
-* help - Display help with commands.
-* read - Displays information about the PDF, including title, subject, author and page numbers. For reading the text of a PDF, use [PDFMiner](https://pypi.org/project/pdfminer/).
+* help - Display help with commands including example usage.
+* read - Displays metadata information about the PDF, including title, subject, author and page numbers. For reading the text of a PDF, use [PDFMiner](https://pypi.org/project/pdfminer/).
 * merge - Merges multiple PDF files in order together into one new PDF file.
-* split - Splits a PDF file into multiple files at a certain page.
+* split - Splits a PDF file into multiple files at certain pages.
